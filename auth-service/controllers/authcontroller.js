7e5fb8500ff88b476afca78fc46d4a6f841d0f29
Harlod Bombala <h.bombala@student.avans.nl>
2026-01-20 16:45:10 +0100

    created gateway and auth service

---
+++ auth-service/controllers/authcontroller.js
@@ -0,0 +1,9 @@
+const User = require('../models/user')
+
+function loginUser(req, res) {
+    let username = req.body.username
+    let password = req.body.password
+
+    
+
+}
---
+++ auth-service/index.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express()
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 3001;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+
